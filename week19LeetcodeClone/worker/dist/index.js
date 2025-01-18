678705487bd921aa23ed40b1f808464b2b73c3f2
parthmern <parthmern@gmail.com>
2025-01-18 13:38:15 -0500

    worker

--- week19LeetcodeClone/worker/dist/index.js
+++ week19LeetcodeClone/worker/dist/index.js
@@ -1,39 +1,42 @@
-import { createClient } from "redis";
-const client = createClient();
-
-async function main() {
-    await client.connect();
-    while(1){
-        const res = await client.brPop("problems", 0);  // donot return null, it wait until get something in queue
-        console.log(res);
-    }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const redis_1 = require("redis");
+const client = (0, redis_1.createClient)();
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.connect();
+        while (1) {
+            const res = yield client.brPop("problems", 0);
+            console.log(res);
+        }
+    });
 }
-
 main();
-
-
 // import { createClient } from "redis";
 // const client = createClient();
-
 // async function processSubmission(submission: string) {
 //     const { problemId, code, language } = JSON.parse(submission);
-
 //     console.log(`Processing submission for problemId ${problemId}...`);
 //     console.log(`Code: ${code}`);
 //     console.log(`Language: ${language}`);
 //     // Here you would add your actual processing logic
-
 //     // Simulate processing delay
 //     await new Promise(resolve => setTimeout(resolve, 1000));
 //     console.log(`Finished processing submission for problemId ${problemId}.`);
 // }
-
 // async function startWorker() {
-
 //     try {
 //         await client.connect();
 //         console.log("Worker connected to Redis.");
-
 //         // Main loop 
 //         // infinite checking that IS there anything in queue ?
 //         while (true) {
@@ -51,6 +54,4 @@ main();
 //         console.error("Failed to connect to Redis", error);
 //     }
 // }
-
 // startWorker();
-
